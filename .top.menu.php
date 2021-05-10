c58103109193f80f32cc8851297816426c4650d7
konstantin zhilyaev <k926227@gmail.com>
2021-05-10 11:15:30 +0300

    ~1.5~ Создан раздел 'Партнерам' с подразделами и страницами.

--- .top.menu.php
+++ .top.menu.php
@@ -34,6 +34,13 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"" 
-	)
+	),
+    Array(
+        "Партнерам",
+        "partners/",
+        Array(),
+        Array(),
+        ""
+    ),
 );
 ?>
---
+++ partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"./", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/shedule-events/", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partners/how-become-partner.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
