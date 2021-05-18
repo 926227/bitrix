087c4bdf917a797dfef88c649a5915625d1d8f5d
konstantin zhilyaev <k926227@gmail.com>
2021-05-18 22:20:35 +0300

    ~1.2~ Создано меню о магазине (в подвале) на основе собственного шаблона.

--- local/templates/.default/include/footer.php
+++ local/templates/.default/include/footer.php
@@ -1,13 +1,30 @@
 <div class="ft_footer">
 			<div class="ft_container">
 				<div class="ft_about">
-					<h4>О магазине</h4>
-					<ul>
+					<h4><?=GetMessage('ABOUT_SHOP')?></h4>
+          <?$APPLICATION->IncludeComponent(
+					"bitrix:menu",
+					"bottom_about",
+					Array(
+						"ALLOW_MULTI_SELECT" => "N",
+						"CHILD_MENU_TYPE" => "",
+						"DELAY" => "N",
+						"MAX_LEVEL" => "1",
+						"MENU_CACHE_GET_VARS" => array(0=>"",),
+						"MENU_CACHE_TIME" => "3600",
+						"MENU_CACHE_TYPE" => "N",
+						"MENU_CACHE_USE_GROUPS" => "Y",
+						"ROOT_MENU_TYPE" => "bottom",
+						"USE_EXT" => "N"
+					)
+				);?>
+
+					<!-- <ul>
 						<li><a href="">Отзывы</a></li>
 						<li><a href="">Контакты</a></li>
 						<li><a href="">Руководство</a></li>
 						<li><a href="">История</a></li>
-					</ul>
+					</ul> -->
 				</div>
 				<div class="ft_catalog">
 					<h4>Каталог товаров</h4>
