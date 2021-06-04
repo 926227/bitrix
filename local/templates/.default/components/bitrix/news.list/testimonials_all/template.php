39f91d87af9a8296b431505f2155c454861b32a8
konstantin zhilyaev <k926227@gmail.com>
2021-06-04 14:30:25 +0300

    ~3~ В разделе Отзывы добавлена поддержкф эрмитажа

--- local/templates/.default/components/bitrix/news.list/testimonials_all/template.php
+++ local/templates/.default/components/bitrix/news.list/testimonials_all/template.php
@@ -14,7 +14,12 @@ $this->setFrameMode(true);
 ?>
 
 <?foreach($arResult["ITEMS"] as $arItem):?>
-	<div class="review-block">
+	<?
+	$this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+	$this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+	
+	<div class="review-block" id="<?=$this->GetEditAreaId($arItem['ID']);?>">
 		<div class="review-text">
 			<div class="review-block-title"><span class="review-block-name"><a href="#"><?=$arItem["NAME"]?></a></span><span class="review-block-description"><?=$arItem["PROPERTIES"]["POSITION"]["VALUE"]?> <?=$arItem["PROPERTIES"]["COMPANY"]["VALUE"]?></span></div>
 			<div class="review-text-cont">
