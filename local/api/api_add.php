b16c5892464c1944171d0fc74d4175670745cf35
konstantin zhilyaev <k926227@gmail.com>
2021-06-09 22:27:23 +0300

    feat: Добавлен ajax вывод на страницу результатов добавления элементов

--- local/api/api_add.php
+++ local/api/api_add.php
@@ -6,50 +6,59 @@ echo "Проверка...<br>";
 
 if ($_SERVER["REQUEST_METHOD"]<>"GET") die();
 if ($_GET['apikey']<>'RUN2021') die();
-if(!CModule::IncludeModule("iblock")) die();
-
-global $USER;
 
 $iCount = isset($_GET['COUNT']) ? (int) $_GET['COUNT'] : 0;
 $iStep = isset($_GET['STEP']) ? (int) $_GET['STEP'] : 0;
-$iBlockId = 7;
+$iBlockId = isset($_GET['IBLOCK']) ? (int) $_GET['IBLOCK'] : 7;
 
 echo "=====<br>";
 echo "Задача: создать $iCount элементов, шаг:$iStep <br>";
 
-$el = new CIBlockElement;
-$i=0;
 
-while ($i < $iCount) {
-  if ($i % $iStep === 0) {
-    echo "добавлены очередные элементы  + $iStep штуки<br>";
+
+?>
+<div id="board">
+  <h3>Добавление информации в информационный блок.</h3>
+</div>
+
+<script>
+const pushElements = async () =>{
+  const count = <?=$iCount?>;
+  const step = <?=$iStep?>;
+  const iBlock = <?=$iBlockId?>;
+  let n = 0;
+  let added = 0;
+
+  if (step < 0 || count < 0) {
+    const info = document.createElement('p');
+    info.textContent = 'Неверные значения показателей';
+    info.setAttribute('style', 'color:red;')
+    board.append(info);
+    return;
   }
 
-  $arProps = Array();
-  $arProps['CITY'][0] = "Город #$i";
-  $arProps['CITY'][1] = "Страна #$i";
-  $arProps['CITY'][2] = "Регион #$i";
+  while (n < count) {
+    const info = document.createElement('p');
+    info.textContent = 'Добавляем элементы... ';
+    board.append(info);
 
-  $arLoadArray = Array(
-    "MODIFIED_BY"    => $USER->GetID(), // элемент изменен текущим пользователем
-    "IBLOCK_SECTION_ID" => false,          // элемент лежит в корне раздела
-    "IBLOCK_ID"      => $iBlockId,
-    "PROPERTY_VALUES"=> $arProps,
-    "NAME"           => "Тест материал #$i",
-    "CODE"           => Cutil::translit("Тест материал #$i", "ru"),
-    "ACTIVE"         => "Y",            // активен
-    "PREVIEW_TEXT"   => "Добавлено с помощью скрипта",
-    "DETAIL_TEXT"    => "Добавлено с помощью скрипта",
-    );
+    const response = await fetch(`./iblock_add.php/?IBLOCK2=${iBlock}&STEP=${step}&COUNT=${count}&start=${n}`);
+    const result = await response.json();
 
-  $i++;  
+    info.append(`добавлено ${result.elementsAdded} элементов.`)
 
-  $el->Add($arLoadArray);
+    added += result.elementsAdded;
+    n += step;
+  }
 
+  info = document.createElement('p');
+  info.textContent = `Задача выполнена! Добавлено элементов: ${added}`;
+  board.append(info);
 }
-echo "=====<br>";
-echo "Задача выполнена..."
-?>
+pushElements();
+</script>
+
+
 
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
