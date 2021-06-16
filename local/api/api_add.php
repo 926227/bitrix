64648ff7ae74fbf335f85f11a208c78587f4ac9a
konstantin zhilyaev <k926227@gmail.com>
2021-06-16 22:19:57 +0300

    feat: Добавлены проверки: сессии, ajax, доработан ajax запрос, переименованы магические константы

--- local/api/api_add.php
+++ local/api/api_add.php
@@ -1,52 +1,75 @@
 <?
-if ($_SERVER["REQUEST_METHOD"]<>"GET") die();
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+if ($_SERVER["REQUEST_METHOD"]<>"GET") exit();
+if (!check_bitrix_sessid()) {
+  header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
+  exit();
+};
 
+const KEY_TO_START_SCRIPT = 'RUN2021';
 global $USER;
-$iCountMax = isset($_GET['COUNT']) ? (int) $_GET['COUNT'] : 0;
-$iStep = isset($_GET['STEP']) ? (int) $_GET['STEP'] : 0;
-$iBlockId = isset($_GET['IBLOCK']) ? (int) $_GET['IBLOCK'] : 7;
-$arAdd = ["addTotal" => 0, "addStep" => 0];
 
-if ($_GET['apikey']<>'RUN2021') {
-  echo "Неверный ключ";
-  die();
+$oInstance = \Bitrix\Main\Application::getInstance();
+$oContext = $oInstance->getContext();
+$oRequest = $oContext->getRequest();
+
+if (!$oRequest->isAjaxRequest()) {
+  echo "Должен быть ajax запрос";
+  exit();
 }
 
-if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) require($_SERVER["DOCUMENT_ROOT"]."/bitrix/modules/main/include/prolog_before.php");
+$sCheckStartScriptKey = $oRequest->getQuery("apikey");
+
+if( $sCheckStartScriptKey <> KEY_TO_START_SCRIPT){
+  header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
+  echo "wrong apikey";
+  exit();
+}
 
 CModule::IncludeModule("iblock");
-$el = new CIBlockElement;
+$oInfoBlockElement = new CIBlockElement;
 
-$i = 0;
-while ($i < $iCountMax) {
-  
+$iTotalNumberOfElementsToAdd = $oRequest->getQuery("count") ? (int) $oRequest->getQuery("count") : 0;
+$iStepNumberOfElementsToAdd = $oRequest->getQuery("step") ? (int) $oRequest->getQuery("step") : 1;
+$iIBlockId = $oRequest->getQuery("iblock") ? (int) $oRequest->getQuery("iblock") : 7;
+$arAddElementsCounter = ["addTotal" => 0, "addDuringStep" => 0];
+
+
+if ($iTotalNumberOfElementsToAdd < 1 || $iStepNumberOfElementsToAdd < 1 || $iTotalNumberOfElementsToAdd < $iStepNumberOfElementsToAdd){
+  header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
+  exit();
+}
+
+$iProposeElementNumber = 0 ;
+while ($arAddElementsCounter["addTotal"] < $iTotalNumberOfElementsToAdd) {
+
   $arProps = Array();
-  $arProps['CITY'][0] = "Город #$i";
-  $arProps['CITY'][1] = "Страна #$i";
-  $arProps['CITY'][2] = "Регион #$i";
+  $arProps['CITY'][0] = "Город #$iProposeElementNumber";
+  $arProps['CITY'][1] = "Страна #$iProposeElementNumber";
+  $arProps['CITY'][2] = "Регион #$iProposeElementNumber";
   
   $arLoadArray = Array(
     "MODIFIED_BY"    => $USER->GetID(), // элемент изменен текущим пользователем
     "IBLOCK_SECTION_ID" => false,          // элемент лежит в корне раздела
-    "IBLOCK_ID"      => $iBlockId,
+    "IBLOCK_ID"      => $iIBlockId,
     "PROPERTY_VALUES"=> $arProps,
-    "NAME"           => "Тест материал #$i",
-    "CODE"           => Cutil::translit("Тест материал #$i", "ru"),
+    "NAME"           => "Тест материал #$iProposeElementNumber",
+    "CODE"           => Cutil::translit("Тест материал #$iProposeElementNumber", "ru"),
     "ACTIVE"         => "Y",            // активен
     "PREVIEW_TEXT"   => "Добавлено с помощью скрипта",
     "DETAIL_TEXT"    => "Добавлено с помощью скрипта",
   );
-  if ($el->Add($arLoadArray)) {
-    $arAdd["addTotal"]++;
-    $arAdd["addStep"]++;
+  if ($oInfoBlockElement->Add($arLoadArray)) {
+    $arAddElementsCounter["addTotal"]++;
+    $arAddElementsCounter["addDuringStep"]++;
   }
-  $i++;  
 
-  if ($i >0 && ($i % $iStep === 0 || $i === $iCountMax)) {
-    echo json_encode(['addTotal' => $arAdd['addTotal']])."*";
+  if ($arAddElementsCounter["addDuringStep"] > 0 && ($arAddElementsCounter["addDuringStep"] >= $iStepNumberOfElementsToAdd ||  $arAddElementsCounter["addTotal"] === $iTotalNumberOfElementsToAdd)) {
+    echo json_encode(['addTotal' => $arAddElementsCounter['addTotal']])."*";
     ob_end_flush();
     flush();
-    $arAdd["addStep"] = 0;
+    $arAddElementsCounter["addDuringStep"] = 0;
   }
+
+  $iProposeElementNumber++;
 }
-echo json_encode(['addTotal' => $arAdd['addTotal']]);
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -3,5 +3,8 @@
 if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/include/functions.php")) {
     require_once ($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/include/functions.php");
 }
+if (file_exists($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/include/external-js.php")) {
+    require_once ($_SERVER["DOCUMENT_ROOT"] . "/local/php_interface/include/external-js.php");
+}
 
 
